f44ab01c96d18a93ac7f571f95fe12d3e2f99cd2
odedia <odedia@gmail.com>
2021-12-02 18:17:18 +0200

    Update SensorsApplication.java

--- src/main/java/org/tanzu/demo/SensorsApplication.java
+++ src/main/java/org/tanzu/demo/SensorsApplication.java
@@ -2,6 +2,8 @@ package org.tanzu.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 
 @SpringBootApplication
 public class SensorsApplication {
